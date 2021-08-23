ce59bb3e60cae0f5e353a5f7e5c45e16a9492747
Naldiln <georgina.toldy@gmail.com>
2021-08-23 22:55:55 +0200

    working javascript

---
+++ images.js
@@ -0,0 +1,102 @@
+const images = [
+  {
+    "_id": "0",
+    "src": "photo0.jpg",
+    "alt": "First photo",
+    "title": "First photo",
+    "description": "The first photo og the bunch"
+  },
+  {
+    "_id": "1",
+    "src": "photo1.jpg",
+    "alt": "Second photo",
+    "title": "Second photo",
+    "description": "The second photo og the bunch"
+  },
+  {
+    "_id": "2",
+    "src": "photo2.jpg",
+    "alt": "Third photo",
+    "title": "Third photo",
+    "description": "The third photo og the bunch"
+  },
+  {
+    "_id": "3",
+    "src": "photo3.jpg",
+    "alt": "Fourth photo",
+    "title": "Fourth photo",
+    "description": "The fourth photo og the bunch"
+  },
+  {
+    "_id": "4",
+    "src": "photo4.jpg",
+    "alt": "Fifth photo",
+    "title": "Fifth photo",
+    "description": "The fifth photo og the bunch"
+  },
+  {
+    "_id": "5",
+    "src": "photo5.jpg",
+    "alt": "Sixth photo",
+    "title": "Sixth photo",
+    "description": "The sixth photo og the bunch"
+  },
+  {
+    "_id": "6",
+    "src": "photo6.jpg",
+    "alt": "Sixth photo",
+    "title": "Sixth photo",
+    "description": "The sixth photo og the bunch"
+  },
+  {
+    "_id": "7",
+    "src": "photo7.jpg",
+    "alt": "Sixth photo",
+    "title": "Sixth photo",
+    "description": "The sixth photo og the bunch"
+  },
+  {
+    "_id": "8",
+    "src": "photo8.jpg",
+    "alt": "Sixth photo",
+    "title": "Sixth photo",
+    "description": "The sixth photo og the bunch"
+  },
+  {
+    "_id": "9",
+    "src": "photo9.jpg",
+    "alt": "Sixth photo",
+    "title": "Sixth photo",
+    "description": "The sixth photo og the bunch"
+  },
+  {
+    "_id": "10",
+    "src": "photo10.jpg",
+    "alt": "Sixth photo",
+    "title": "Sixth photo",
+    "description": "The sixth photo og the bunch"
+  },
+  {
+    "_id": "11",
+    "src": "photo11.jpg",
+    "alt": "Sixth photo",
+    "title": "Sixth photo",
+    "description": "The sixth photo og the bunch"
+  },
+  {
+    "_id": "12",
+    "src": "photo12.jpg",
+    "alt": "Sixth photo",
+    "title": "Sixth photo",
+    "description": "The sixth photo og the bunch"
+  },
+  {
+    "_id": "13",
+    "src": "photo13.jpg",
+    "alt": "Sixth photo",
+    "title": "Sixth photo",
+    "description": "The sixth photo og the bunch"
+  }
+]
+
+export default images;
---
+++ main.js
@@ -0,0 +1,59 @@
+import images from "./images.js";
+
+console.log(images);
+
+images.map(image => {
+  const item = document.createElement("div");
+  item.classList.add("item");
+  $(".thumbnails").append(item);
+
+  const caption = document.createElement("p");
+  caption.classList.add("caption");
+  $(caption).text(`${image.title}`)
+  $(item).append(caption);
+
+  const itemImg = document.createElement("img");
+  itemImg.classList.add("thumb");
+  itemImg.data("index", `${image._id}`);
+  $(itemImg).attr("src", `./img/${image.src}`);
+  $(item).append(itemImg);
+});
+
+
+let curImg = 0;
+
+console.log(images.length);
+
+const loadPhoto = imgNum => {
+  $(".photo").attr("src", `./img/${images[imgNum].src}`);
+  $(".title").text(`${images[imgNum].title}`);
+  $(".description").text(`${images[imgNum].description}`);
+}
+
+$(".arrow-right").on("click", () => {
+  if (curImg < images.length - 1) {
+    loadPhoto(++curImg);
+  } else {
+    curImg = images[0]._id;
+    loadPhoto(curImg);
+  }
+});
+
+$(".arrow-left").on("click", () => {
+  if (curImg > 0) {
+    loadPhoto(--curImg);
+  } else {
+    curImg = images[images.length - 1]._id;
+    loadPhoto(curImg);
+  }
+});
+
+$(".caption").on("click", (event) => {
+  let clicked = $(event.target).attr("data-index");
+  console.log(clicked);
+  let imgNum = parseInt(clicked);
+  console.log(imgNum);
+  $(".photo").attr("src", `./img/${images[imgNum].src}`);
+})
+
+loadPhoto(curImg);
